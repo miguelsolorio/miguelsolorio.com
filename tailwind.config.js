f750c591fd7f9631ae3d8feec99a5b1951cfca2c
Miguel Solorio <miguelsolorio@fb.com>
2022-12-26 08:04:56 -0800

    Clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,47 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        dark: { raw: '(prefers-color-scheme: dark)' },
+        dark: {
+          raw: '(prefers-color-scheme: dark)'
+        },
       },
+      animation: {
+        gradientX: 'gradientX 15s ease infinite',
+        gradientY: 'gradientY 15s ease infinite',
+        gradientXY: 'gradientXY 15s ease infinite',
+      },
+      keyframes: {
+        gradientY: {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'center top'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'center center'
+          }
+        },
+        gradientX: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
+        },
+        gradientXY: {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
+        }
+      }
     },
   },
   variants: {},
